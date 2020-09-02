708c4ffad539aad5c7b40cc0df2ccf6d97155662
Michael Dotson <thedotson@icloud.com>
2020-09-01 20:04:54 -0500

    auth function works

--- src/App/App.js
+++ src/App/App.js
@@ -1,13 +1,45 @@
 import React from 'react';
+import firebase from 'firebase/app';
+import 'firebase/auth';
+// import authData from '../helpers/data/authData';
+import fbConnection from '../helpers/data/connection';
+import Navbar from '../components/pages/Navbar/Navbar';
 import './App.scss';
 
+fbConnection();
+
 class App extends React.Component {
+  state = {
+    authed: false,
+  }
+
+  componentDidMount() {
+    this.listener = firebase.auth().onAuthStateChanged((user) => {
+      user ? this.setState({ authed: true }) : this.setState({ authed: false });
+    });
+  }
+
+  componentWillUnmount() {
+    this.listener();
+  }
+
   render() {
+    const { authed } = this.state;
+
+    // const loadComponent = () => {
+    //   if (authed) {
+    //     const uid = authData.getUid();
+    //     return ;
+    //   }
+    //   return (
+    //     <h2 className="text-center">Please login to view the roster</h2>
+    //   );
+    // }
+
     return (
       <div className="App">
         <h1>React-Hoarder</h1>
-        <button className="btn btn-danger">
-        <i className="fas fa-gem"></i> Hoard <i className="fas fa-gem"></i></button>
+        <Navbar authed={authed}/>
       </div>
     );
   }
